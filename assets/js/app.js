ef7bed4d7cf81c84f2d3f127348aa88ac917fe08
jesus <ingjesussantiago@gmail.com>
2022-11-02 13:42:28 -0600

    agragando archivo js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 class Hamburguesa {
     constructor(nombre, ingrediente, precio){
         this.nombre = nombre
@@ -6,6 +14,11 @@ class Hamburguesa {
     }
 }
 
-const Hamburguesa_clásica = new Hamburguesa("Hamburguesa Clásica","Medallón de Carne, Salchicha, Queso Amarillo, Jamón, Lechuga, Tomate, Cebolla y Chile.", "$ "+55);
+const Hamburguesa_clásica = new Hamburguesa ( "Hamburguesa Clásica","Medallón de Carne, Salchicha, Queso Amarillo, Jamón, Lechuga, Tomate, Cebolla y Chile.", "$ "+55)
+const Hamburguesa_Hawaiana = new Hamburguesa("Hamburguesa Hawaiana", "Medallón de Carne, Salchicha, Queso Manchego, Queso Amarillo, Tocino, Jamón, Lechuga, Tomate, Cebolla Piña y Chile.", "$ "+65);
+const Hamburguesa_Especial = new Hamburguesa("Hamburguesa Doble", "Doble Medallón de Carne, Salchicha, Queso Manchego, Queso Amarillo, Tocino, Jamón, Lechuga, Tomate, Cebolla y Chile.", "$ "+ 80);
 
-console.log("hamburguesa_clasica");
+
+const Hamburguesas = [
+    Hamburguesa_clásica,Hamburguesa_Hawaiana,Hamburguesa_clásica
+]
