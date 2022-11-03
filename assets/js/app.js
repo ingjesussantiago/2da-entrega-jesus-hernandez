0dda7e3ed97b94f415e7f9c26fd57b5bb5ed7cf3
jesus <ingjesussantiago@gmail.com>
2022-11-02 19:53:50 -0600

    fin de dia

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,6 @@ class Hamburguesa {
     }
 }
 
-const Hamburguesa_clásica = new Hamburguesa("Hamburguesa Clásica","Medallón de Carne, Salchicha, Queso Amarillo, Jamón, Lechuga, Tomate, Cebolla y Chile.", "$ "+55);
-
-console.log("hamburguesa_clasica");
+const Hamburguesa_clásica = new Hamburguesa("Hamburguesa Clásica","Medallón de Carne, Salchicha, Queso Amarillo, Jamón, Lechuga, Tomate, Cebolla y Chile.", 55);
+const Hamburguesa_Hawaiana = new Hamburguesa("Hamburguesa Hawaiana", "Medallón de Carne, Salchicha, Queso Manchego, Queso Amarillo, Tocino, Jamón, Lechuga, Tomate, Cebolla Piña y Chile.", 65);
+const Hamburguesa_Especial = new Hamburguesa("Hamburguesa Doble", "Doble Medallón de Carne, Salchicha, Queso Manchego, Queso Amarillo, Tocino, Jamón, Lechuga, Tomate, Cebolla y Chile.",  80);
