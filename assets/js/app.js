9f27e7d8fcb5eed2d388d56fddc48c9e45c68bd0
jesus <ingjesussantiago@gmail.com>
2022-11-03 19:32:09 -0600

    se acompleta app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,80 +11,87 @@ const Hamburguesa_Hawaiana = new Hamburguesa("Hamburguesa Hawaiana", "Medallón 
 const Hamburguesa_Especial = new Hamburguesa("Hamburguesa Doble", "Doble Medallón de Carne, Salchicha, Queso Manchego, Queso Amarillo, Tocino, Jamón, Lechuga, Tomate, Cebolla y Chile.",  80);
 
 
-let cliente=prompt("ingrese su nombre");
+let cliente=prompt("Ingrese su Nombre por favor");
 let dirección=prompt("Ingrese su dirección");
+const añadiendo_productos =[];
 
 
 function pedido() {
+  
+  opción=prompt("Ingrese el producto que desea  acorde al numero correspondiente:\n1. Hamburguesa clásica  \n2. Hamburguesa Hawaiana \n3. Hamburguesa doble");
+  
+  let cantidad=""
 
-opción=prompt("ingrese el producto que desea  numero correspondiente:\n1. Hamburguesa clásica  \n2. Hamburguesa Hawaiana \n3. Hamburguesa doble  \n(*)si ingresa su valor diferente se ejecutara r=a/b");
-
-let cantidad=""
+ 
     
+  function solicitar(params) {
+    cantidad = prompt("¿Ingrese cantidad de hamburguesas?");
+    }
+
 opción=Number(opción);
 
 switch (opción) {
+
   case 1:
     precio=Hamburguesa_clásica.precio;
-    function solicitar(params) {
-    cantidad = prompt("ingrese la cantidad deseada");
-    }
     solicitar()
-   
+    añadiendo_productos.push (Hamburguesa_clásica.precio*cantidad)
+    console.log(añadiendo_productos);
     break;
-case 2:
+
+  case 2:
     precio=Hamburguesa_Hawaiana.precio;
-    function solicitar(params) {
-    cantidad = prompt("ingrese la cantidad deseada");
-    }
     solicitar()
-  break;
+    añadiendo_productos.push (Hamburguesa_Hawaiana.precio*cantidad)
+    console.log(añadiendo_productos);
+    break;
+
   case 3:
     precio=Hamburguesa_Especial.precio;
-    function solicitar(params) {
-    cantidad = prompt("ingrese la cantidad deseada");
-    }
     solicitar()
+    añadiendo_productos.push (Hamburguesa_Especial.precio*cantidad)
+    console.log(añadiendo_productos);
     break;
+
   default:
-    alert("No solicito Nada");
-    pedido()
-    break;
+   
+      alert("No solicito Nada");
+      pedido()
+      break;
+    
+  
 }
-
-
-
 
 opción2=prompt( "¿Desea agregar algo mas a su cuenta: \n1. si , Seguir Comprando  \n2. No, La cuenta por Favor ");
 
-
 if (opción2==2) {
-
-    Opción3= prompt("Solicita Servicio a Domicilio:\n1. si  \n2. No ")
-
-    if (Opción3==1) {
-        let precio_total= Number(cantidad*(precio)*1.15 + 15).toFixed(2);
-         alert("Estimado "+ cliente + " Su cuenta es de "+ precio_total + " y se entrega a domicilio en la siguiente dirección "+ dirección );
-    }
-    if (Opción3==2) {
-        let precio_total= Number(cantidad*(precio)*1.15).toFixed(2);
-          alert("Estimado "+ cliente + " Su cuenta es de "+ precio_total + " y pasa a recoger su pedido");
-    }
-
+  
+  Opción3= prompt("Solicita Servicio a Domicilio:\n1. si  \n2. No ")
+  
+  if (Opción3==1) {
+    const total = añadiendo_productos.reduce((acumulador, elemento) => acumulador + elemento, 0)
+    console.log(total);
+    totalmasiva = total+15
+    alert("Estimado(a) "+ cliente +" Su cuenta es de "+ totalmasiva + " y se entrega en la siguiente dirección: " + dirección )
+    
+  }
+  if (Opción3==2) {
+    const total = añadiendo_productos.reduce((acumulador, elemento) => acumulador + elemento, 0)
+    console.log(total);
+    totalmasiva = total
+    alert("Estimado(a) "+ cliente +" Su cuenta es de "+ totalmasiva + " y se entrega en sucursal" )
+  }
+  
   
 }
 
 if (opción2 == 1) {
-    console.log("pendiente");
     pedido()
-    
 }
 
- 
 }
 
 pedido();
-
 
 
 
